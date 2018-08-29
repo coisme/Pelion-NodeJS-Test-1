9bfcaa3cc482f2fe5904bf20268344ed8b5b483e
Osamu Koizumi <Osamu.Koizumi@arm.com>
2018-08-30 01:11:15 +0900

    Change print format.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var dateformat = require('dateformat');
 var MbedCloudSDK = require("mbed-cloud-sdk");
 
 var connect = new MbedCloudSDK.ConnectApi({
-    apiKey: "<< Your API Key >>"
+    apiKey: "<< Your API key >>"
 });
 
 var deviceId = "<< Your Device ID >>";
@@ -9,13 +10,13 @@ var resourceURI = "3200/0/5501";  // Button Count
 
 var period = 5000; // ms
 
-var callCount = 0;
+var retCount = 0;
 
 function getDeviceResource(){
     connect.getResourceValue(deviceId, resourceURI)
     .then(data =>{
-        callCount += 1;
-        console.log(callCount + ": " + data);
+        retCount += 1;
+        console.log(dateformat(new Date(), 'isoTime') + ", cnt " + retCount + ", btn " + data);
     })
     .catch(error =>{
         console.log(error);
